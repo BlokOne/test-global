fd41cfe9d62bea1172f9a7521688468325b18f89
BlokOne <AFKloungebar@icloud.com>
2022-08-04 16:05:14 +0300

    test

---
+++ src/components/main.js
@@ -0,0 +1,30 @@
+const iconMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.header__navigation');
+const buttonSub = document.querySelector('.head__input-button')
+const input = document.querySelector('input')
+const errorArea = document.querySelector('.head__error')
+const description = document.querySelector('.head__description')
+
+
+if (iconMenu) {
+   iconMenu.addEventListener("click", function (e) {
+      iconMenu.classList.toggle('active');
+      menuBody.classList.toggle("active");
+   });
+}
+
+buttonSub.addEventListener("click", () => {
+   errorArea.innerHTML = ""
+   if (input.value.match(/[!@#$%^&*()]+/)) {
+      errorArea.innerHTML = "prohibition on entering characters !@#$%^&*()"
+   } else {
+      input.value = "";
+      alert('Submit Done')
+   }
+})
+
+fetch("https://baconipsum.com/api/?type=lucky").then(function (response) {
+   response.text().then(function (text) {
+      description.innerHTML = text.slice(1).slice(0, -1)
+   });
+})
---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+// const ESLintPlugin = require('eslint-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    return {
+        minimize: isProd,
+        splitChunks: {
+            chunks: 'async',
+            minSize: 20000,
+            minRemainingSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    reuseExistingChunk: true,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
+    };
+}
+
+module.exports = {
+    context: path.resolve(__dirname, ''),
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: optimization(),
+    devServer: {
+        static: './src',
+        hot: true,
+        port: 9000,
+        historyApiFallback: true,
+    },
+    devtool: isDev ? 'eval' : false,
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src/assets/img/'),
+                to: path.resolve(__dirname, 'dist/assets/img')
+            }
+        ]),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                // loader: {
+                //     loader: 'babel-loader',
+                //     options: {
+                //         presets: [
+                //             '@babel/preset-env',
+                //             // '@babel/preset-react' Раскомментировать и установить зависимость, если хотите использовать React
+                //         ]
+                //     }
+                // }
+            },
+            {
+                test: /\.(s[ac]ss)$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                }, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|webp)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ],
+
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot|)$/,
+                use: ['file-loader']
+            },
+        ]
+    }
+};
